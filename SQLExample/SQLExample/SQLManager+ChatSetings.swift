eb2ce0bcd0978acf78fc6fba8970b4f673119eab
CainLuo <350116542@qq.com>
2021-10-08 17:40:48 +0800

    Update SQLManager+ChatSetings.swift

--- SQLExample/SQLExample/SQLManager+ChatSetings.swift
+++ SQLExample/SQLExample/SQLManager+ChatSetings.swift
@@ -12,8 +12,9 @@ import SQLite
 fileprivate let chatSetings = Table("usersChatSetings")
 
 // Expressions
-fileprivate let id    = Expression<String>("id")
-fileprivate let price = Expression<String>("price")
+fileprivate let index  = Expression<Int64>("index")
+fileprivate let userID = Expression<String>("userID")
+fileprivate let price  = Expression<String>("price")
 
 extension SQLManager {
     /// 创建用户表
@@ -21,8 +22,9 @@ extension SQLManager {
     func createChatSettingsTable(_ db: Connection) {
         do {
             try db.run(chatSetings.create(ifNotExists: true) { t in
+                t.column(index, primaryKey: .autoincrement)
                 // autoincrement：自动递增
-                t.column(id, unique: true)
+                t.column(userID, unique: true)
                 // unique：用于保证多列中是唯一值
                 t.column(price, defaultValue: "0")
             })
@@ -35,9 +37,9 @@ extension SQLManager {
     /// - Parameters:
     ///   - userID: String
     ///   - chat: UserChatSettingModel
-    func insetChatSetings(_ userID: String, chat: UserChatSettingModel) {
+    func insetChatSetings(_ uUserID: String, chat: UserChatSettingModel) {
         do {
-            try db.run(chatSetings.insert(id <- userID, price <- chat.price))
+            try db.run(chatSetings.insert(userID <- uUserID, price <- chat.price))
         } catch {
             print("💥💥💥 -------------- \(error.localizedDescription) -------------- 💥💥💥")
         }
@@ -47,10 +49,10 @@ extension SQLManager {
     /// - Parameters:
     ///   - userID: String
     ///   - chat: UserChatSettingModel
-    func updateChatSetting(_ userID: String, chat: UserChatSettingModel) {
-        let user = chatSetings.filter(id == userID)
+    func updateChatSetting(_ uUserID: String, chat: UserChatSettingModel) {
+        let user = chatSetings.filter(userID == uUserID)
         do {
-            try db.run(user.update(id <- userID, price <- chat.price))
+            try db.run(user.update(userID <- uUserID, price <- chat.price))
         } catch {
             print("💥💥💥 -------------- \(error.localizedDescription) -------------- 💥💥💥")
         }
@@ -58,8 +60,8 @@ extension SQLManager {
     
     /// 删除用户在ChatSettings表的子信息
     /// - Parameter userID: String
-    func deleteChatSeting(_ userID: String) {
-        let userInfo = chatSetings.filter(id == userID)
+    func deleteChatSeting(_ uUserID: String) {
+        let userInfo = chatSetings.filter(userID == uUserID)
         do {
             if try db.run(userInfo.delete()) > 0 {
                 print("👍🏻👍🏻👍🏻 -------------- 删除所有用户成功 -------------- 👍🏻👍🏻👍🏻")
@@ -73,8 +75,8 @@ extension SQLManager {
     
     /// 获取ChatSettings表的子信息
     /// - Parameter userID: String
-    func getChatSetting(_ userID: String, complete: ((_ chatSetting: UserChatSettingModel) -> Void)) {
-        let query = chatSetings.filter(id == userID)
+    func getChatSetting(_ uUserID: String, complete: ((_ chatSetting: UserChatSettingModel) -> Void)) {
+        let query = chatSetings.filter(userID == uUserID)
         do {
             try db.prepare(query).forEach({ user in
                 complete(UserChatSettingModel(price: user[price]))
